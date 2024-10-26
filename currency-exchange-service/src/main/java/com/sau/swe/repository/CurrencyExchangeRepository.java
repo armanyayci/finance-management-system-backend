bb297c03549fb1f3838b84054443898edf214c30
Ahmet Alıç <ahmet.alic@ogr.sakarya.edu.tr>
2024-10-27 02:03:46 +0300

    feat: implement core components for currency exchange service
    - Added CurrencyExchange entity, DTOs, service and repository
    - Created CurrencyExchangeController for handling API requests

---
+++ currency-exchange-service/src/main/java/com/sau/swe/repository/CurrencyExchangeRepository.java
@@ -0,0 +1,7 @@
+package com.sau.swe.repository;
+
+import com.sau.swe.entity.CurrencyExchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange,Long> {
+}
---
+++ utils-module/src/main/java/com/sau/swe/utils/exception/CurrencyExchangeException.java
@@ -0,0 +1,7 @@
+package com.sau.swe.utils.exception;
+
+public class CurrencyExchangeException extends RuntimeException{
+    public CurrencyExchangeException(String message) {
+        super(message);
+    }
+}
