d715e07cd606f683893266d1b73ad02997e4e0c3
cebo54 <kayacebrail1905@gmail.com>
2024-11-09 01:28:32 +0300

    Token generation done.

---
+++ account-service/src/main/java/com/sau/swe/dto/BalanceRequest.java
@@ -0,0 +1,15 @@
+package com.sau.swe.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class BalanceRequest {
+    private Long accountId;
+    private double amount;
+}
