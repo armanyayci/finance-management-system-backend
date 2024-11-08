24b6e601b2d32b1b74d4f4888e8974418fa91703
cebo54 <kayacebrail1905@gmail.com>
2024-11-09 01:28:32 +0300

    Token generation done.

---
+++ account-service/src/main/java/com/sau/swe/dto/TransferRequest.java
@@ -0,0 +1,16 @@
+package com.sau.swe.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TransferRequest {
+    private Long senderId;
+    private String code;
+    private Long money;
+}
