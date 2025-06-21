15df94f1a0b7422772fbd2abe9cca2f59a003de6
armanyayci <armanyayci@icloud.com>
2025-06-21 18:28:22 +0300

    entities added

---
+++ account-service/src/main/java/com/sau/swe/entity/Transaction.java
@@ -0,0 +1,43 @@
+package com.sau.swe.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Table(name = "TRANSACTIONS")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY,generator = "transaction_seq_generator")
+    @SequenceGenerator(name = "transaction_seq_generator", sequenceName = "transactions_sequence", allocationSize = 1)
+    private Long id;
+
+    @Column(name = "amount")
+    private Long amount;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "payment_type")
+    @Enumerated(EnumType.STRING)
+    private PaymentType paymentType;
+
+    @Column(name = "transaction_time")
+    private LocalDateTime transactionTime;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<AccountActivities> accountActivities;
+
+    public enum PaymentType {
+        CASH,
+        CREDIT_CARD,
+        EXCHANGE
+    }
+}
