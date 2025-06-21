b3f797388d2dab8501020105113b3dc5fb52d92f
cebo54 <kayacebrail1905@gmail.com>
2025-06-21 21:32:37 +0300

    Docker implementation started.

--- authentication-service/src/main/java/com/sau/swe/config/AppConfig.java
+++ authentication-service/src/main/java/com/sau/swe/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.sau.swe.config;
 
 import com.sau.swe.repository.UsersRepository;
+
 import com.sau.swe.utils.exception.GenericFinanceException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
