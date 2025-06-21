980d81d699f0899c1d1eac8ba1f27de1631005b0
armanyayci <armanyayci@icloud.com>
2025-06-21 21:35:39 +0300

    conflict fix

--- api-gateway/src/main/java/com/sau/swe/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/sau/swe/apigateway/ApiGatewayApplication.java
@@ -1,14 +1,19 @@
 package com.sau.swe.apigateway;
 
 
+import com.sau.swe.utils.Constants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
+        SpringApplication app = new SpringApplication(ApiGatewayApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", Constants.API_GATEWAY_RUN_PORT));
+        app.run(args);
     }
 
 }
--- discovery-server/src/main/java/com/sau/swe/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/sau/swe/discoveryserver/DiscoveryServerApplication.java
@@ -1,14 +1,19 @@
 package com.sau.swe.discoveryserver;
 
 
+import com.sau.swe.utils.Constants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServerApplication.class, args);
+        SpringApplication app = new SpringApplication(DiscoveryServerApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", Constants.DISCOVERY_SERVER_RUN_PORT));
+        app.run(args);
     }
 
 }
