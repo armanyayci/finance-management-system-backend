16e49e17c0471f9234f4d1ff721e07f0979eeb55
cebo54 <kayacebrail1905@gmail.com>
2025-06-24 00:58:22 +0300

    Docker implementation last version

--- config-service/src/main/java/com/sau/swe/Main.java
+++ config-service/src/main/java/com/sau/swe/Main.java
@@ -1,7 +1,17 @@
 package com.sau.swe;
 
+import com.sau.swe.utils.Constants;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import java.util.Collections;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication app = new SpringApplication(Main.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", Constants.CONFIG_RUN_PORT));
+        app.run(args);
     }
 }
